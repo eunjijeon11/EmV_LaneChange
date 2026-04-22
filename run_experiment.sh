ac4fb36038cb3a3f70c7265eb0e969dc68eca854
eunzee <eunjiamy@naver.com>
2026-04-22 14:37:27 +0000

    v2x experiments

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,7 +1,7 @@
-AGENT_TYPE="dqn" # q-learning, sarse, dqn
-SAVE_PATH="weights/DQN/2stage.pth" # path to save model policy
+AGENT_TYPE="q-learning" # q-learning, sarse, dqn
+SAVE_PATH="weights/Q-Learning/2stage.pkl" # path to save model policy
 
 python main.py \
     --agent_type "${AGENT_TYPE}" \
     --save_path "${SAVE_PATH}" \
-    --is_2stage > logs/dqn_2stage.log
+    --is_2stage > logs/q_2stage_V2X_2.log
